9c85868119c373a5ff01a0f628481a0c429b1273
Craig J Horne <hornedesign@outlook.com>
2020-08-12 20:04:56 +0100

    home page completed

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './Header';
+import Navigation from './Navigation';
 import Home from './Home';
 
 function App() {
@@ -11,14 +12,17 @@ function App() {
        <Switch>
         <Route path="/checkout">
           <Header />
+          <Navigation />
           <h1>Checkout</h1>
         </Route>
         <Route path="/login">
           <Header />
+          <Navigation />
           <h1>Login</h1>
         </Route>
         <Route path="/">
           <Header />
+          <Navigation />
           <Home />
         </Route>
        </Switch>
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,120 @@
 import React from 'react';
 import './Home.css';
+import { Link } from 'react-router-dom';
+import CheckCircle from '@material-ui/icons/CheckCircleOutline';
+
 
 function Home() {
   return (
     <div className="home">
+    	
+    	<div className="home__heroContainer">
+
+    		<div className="home__heroMain">
+
+	    		<Link to="/">
+					<img
+						className="home__bannerHero"
+						src={require("./images/Wasps-768x456.jpg")} 
+						alt="logo"
+					/>
+				</Link>
+
+			</div>
+
+			<div className="home__heroSmall">
+
+				<Link to="/">
+					<img
+						className="home__bannerHero"
+						src={require("./images/EPS Small575 x 320.jpg")} 
+						alt="logo"
+					/>
+				</Link>
+
+				<Link to="/">
+					<img
+						className="home__bannerHero"
+						src={require("./images/EPS Small575 x 320.jpg")} 
+						alt="logo"
+					/>
+				</Link>
+
+			</div>
+
+		</div>	
+
+		<div className="home__infoContainer">
+
+			<div className="home__infoOption">
+				<CheckCircle className="home__infoCheck" />
+				<div className="home__infoText">
+					<span>PERSONALISE SELECTED PRODUCTS</span>
+				</div>
+			</div>
+
+			<div className="home__infoOption">
+				<CheckCircle className="home__infoCheck" />
+				<div className="home__infoText">
+					<span>WORLDWIDE DELIVERY</span>
+				</div>
+			</div>
+
+			<div className="home__infoOption">
+				<CheckCircle className="home__infoCheck" />
+				<div className="home__infoText">
+					<span>28 DAY MONEY BACK GUARANTEE</span>
+				</div>
+			</div>
+
+			<div className="home__infoOption">
+				<CheckCircle className="home__infoCheck" />
+				<div className="home__infoText">
+					<span>OFFICIAL CLUB MERCHANDISE</span>
+				</div>
+			</div>
+
+		</div>
+
+		<div className="home__lowerContainer">
+
+			<Link to="/">
+				<img
+					className="home__bannerLower"
+					src={require("./images/EPS Small575 x 320.jpg")} 
+					alt="logo"
+				/>
+			</Link>
+
+			<Link to="/">
+				<img
+					className="home__bannerLower"
+					src={require("./images/EPS Small575 x 320.jpg")} 
+					alt="logo"
+				/>
+			</Link>
+
+			<Link to="/">
+				<img
+					className="home__bannerLower home__bannerRemove"
+					src={require("./images/EPS Small575 x 320.jpg")} 
+					alt="logo"
+				/>
+			</Link>
+
+		</div>
+
+		<footer className="home__footer">
+
+			<div className="home__footerText">
+
+				<p>Copyright &#169; Elite Pro Sports Ltd, registered in England. reg no. 05193262.</p>
+				<p>Registered address: Elite Pro Sports, Heavens Walk, Doncaster, DN4 5HZ</p>
+
+			</div>
+
+		</footer>
+
     </div>
   );
 }
